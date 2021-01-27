39e88b450425a1bdddf6b45ee7b6cb4dcfb958eb
Daniela Vazquez <daniela_056@hotmail.com>
2021-01-26 18:32:24 -0700

    Add bootstrap to application

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,12 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("bootstrap-sprockets")
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery").start()
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
