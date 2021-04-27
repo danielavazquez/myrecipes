d87f1b7794cbcf21921a282fc7aa660fb6f5ed8f
Daniela Vazquez <daniela_056@hotmail.com>
2021-04-27 10:49:33 -0600

    Add TDDand index listing

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -1,7 +1,23 @@
-require 'test_helper'
+ require 'test_helper'
+ require 'rails-controller-testing'
 
-class RecipesTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  class RecipesTest < ActionDispatch::IntegrationTest
+    
+  def setup
+    @chef = Chef.create!(chefname: "mashrur", email: "mashrur@example.com")
+    @recipe = Recipe.create(name: "vegetable saute", description: "great vegetable sautee, add vegetable and oil", chef: @chef)
+    @recipe2 = @chef.recipes.build(name: "chicken saute", description: "great chicken dish")
+    @recipe2.save
+  end
+  test "should get recipes index" do
+    get recipes_path
+    assert_response :success
+  end
+
+  test "should get recipes listing" do
+    get recipes_path
+    assert_template 'recipes/index'
+    assert_match @recipe.name, response.body  #both recipe names show up in listing index page, want name to be a url
+    assert_match @recipe2.name, response.body
+  end
 end
