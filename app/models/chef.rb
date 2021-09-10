484643e6a3a382c3805bf6e9a11add54524e48d7
daniela <daniela_056@hotmail.com>
2021-09-10 16:03:17 -0600

    Create chatroom migration and message model with template and controllers

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -9,5 +9,6 @@ validates :email, presence: true, length: { maximum: 255 },
 has_many :recipes, dependent: :destroy
 has_secure_password
 validates :password, presence: true, length: { minimum: 5 }, allow_nil: true
+has_many :messages, dependent: :destroy
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_163424) do
+ActiveRecord::Schema.define(version: 2021_09_10_213852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_08_26_163424) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string "name"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.integer "chef_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "recipe_ingredients", force: :cascade do |t|
